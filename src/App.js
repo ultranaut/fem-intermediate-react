0906f8782f2ab07c4bb71a879b6c0b4040c5f45e
John Shearer <john@ultranaut.com>
2022-06-01 16:46:40 -0400

    Split the Details, SearcParams and Modal components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { render } from "react-dom";
-import SearchParams from "./SearchParams";
-import { StrictMode, useState } from "react";
+import { StrictMode, useState, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const App = () => {
   const theme = useState("darkblue");
   return (
     <StrictMode>
-      <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeContext.Provider>
+      <Suspense fallback={<h2>Loading, be patient</h2>}>
+        <ThemeContext.Provider value={theme}>
+          <BrowserRouter>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeContext.Provider>
+      </Suspense>
     </StrictMode>
   );
 };
